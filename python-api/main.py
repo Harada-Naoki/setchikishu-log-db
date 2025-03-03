0a5af5d6340e8f031215a12a2c448e9683105289
Harada-Naoki <naoki_harada@eaglegroup.co.jp>
2025-03-03 17:11:42 +0900

    mainpy

--- python-api/main.py
+++ python-api/main.py
@@ -11,11 +11,20 @@ load_dotenv()
 app = FastAPI()
 
 # MySQL 接続情報
+# DB_CONFIG = {
+#     "host": os.getenv("DB_HOST"),
+#     "user": os.getenv("DB_USER"),
+#     "password": os.getenv("DB_PASSWORD"),
+#     "database": os.getenv("DB_NAME")
+# }
+
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME")
+    "database": os.getenv("DB_NAME"),
+    "port": 4000,
+    "ssl": {"ssl": {}}
 }
 
 # 特殊文字を削除する関数（データの正規化）
